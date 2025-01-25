6f49f0368f06394627ab984b283fe1de4ec0b296
rva174 <rva174@yandex.ru>
2025-01-25 11:38:28 +0500

    init

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
-from fastapi import FastAPI, Path
 
-from app.routers import task, user
+# -*- coding: utf-8 -*-
+from fastapi import FastAPI
+from routers import user
+from routers import task
 
 app = FastAPI()
 
-@app.get('/')
+
+@app.get("/")
 async def welcome():
     return {"message": "Welcome to Taskmanager"}
 
+
 app.include_router(user.router)
 app.include_router(task.router)
-
--- app/routers/user.py
+++ app/routers/user.py
@@ -2,17 +2,19 @@ from fastapi import APIRouter
 
 router = APIRouter(prefix='/user', tags=['user'])
 
-@router.get('/')
+@router.get('/all_users')
 async def all_users():
-    pass
-
-@router.get('/user_id')
-async def user_by_id():
     pass
 
 @router.post('/create')
 async def create_user():
     pass
+
+@router.get('/user_id')
+async def uset_by_id():
+    pass
+
+
 
 @router.put('/update')
 async def update_user():
